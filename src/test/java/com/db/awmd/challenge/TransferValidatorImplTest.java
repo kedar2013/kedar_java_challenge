f58b53b2c12647bdc7383a960eea65ab5e134b01
kedar2013 <kedar.vadgaonkar@gmail.com>
2022-01-26 21:07:09 +0530

    refactoring

--- src/test/java/com/db/awmd/challenge/TransferValidatorImplTest.java
+++ src/test/java/com/db/awmd/challenge/TransferValidatorImplTest.java
@@ -1,9 +1,10 @@
-package com.db.awmd.challenge.service;
+package com.db.awmd.challenge;
 
 import com.db.awmd.challenge.domain.Account;
 import com.db.awmd.challenge.domain.Transfer;
 import com.db.awmd.challenge.exception.AccountNotFoundException;
 import com.db.awmd.challenge.exception.NotEnoughFundsException;
+import com.db.awmd.challenge.service.TransferValidator;
 import com.db.awmd.challenge.service.impl.TransferValidatorImpl;
 import org.junit.Before;
 import org.junit.Test;
